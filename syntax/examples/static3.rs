c9119d184869488425c0a0aba6a329d0e8c7dfca
kindywu <kindywu@outlook.com>
2024-07-04 23:57:12 +0800

    commit

--- syntax/examples/static3.rs
+++ syntax/examples/static3.rs
@@ -1,10 +1,15 @@
 #![allow(unused)]
 
+use std::thread;
+
 fn main() {
     // let linux = "linux".to_string();
     // let pfs = PosixFileSystem { name: &linux };
     let pfs = PosixFileSystem { name: "linux" };
     test_fs(&pfs);
+
+    // let h = thread::spawn(|| println!("spawn"));
+    // h.join();
 }
 
 trait FileSystem: Send + Sync + 'static {}
