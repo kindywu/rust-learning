fc6416dc30d1ebd741e4f3b720fce0481ab4f0fb
kindywu <kindywu@outlook.com>
2024-07-07 16:53:09 +0800

    commit

--- miscellaneous/examples/coroutine.rs
+++ miscellaneous/examples/coroutine.rs
@@ -1,122 +1,114 @@
-#![allow(unused)]
-
-// 实现一个简单的Rust协程库，支持协程的创建、调度和切换。你的实现应该能够处理协程的挂起和恢复，以及协程之间的数据传递。
-
-// struct Coroutine<T> {
-//     // 定义协程的数据结构
-// }
-
-// impl<T> Coroutine<T> {
-//     // 实现协程的创建
-//     fn new<F>(f: F) -> Self
-//     where
-//         F: FnOnce() -> T,
-//     {
-//         // 你的代码
-//     }
-
-//     // 实现协程的运行
-//     fn resume(self) -> T {
-//         // 你的代码
-//     }
-// }
-
-// // 实现协程调度器
-// struct CoroutineScheduler {
-//     // 定义调度器的数据结构
-// }
-
-// impl CoroutineScheduler {
-//     // 实现协程的调度
-//     fn schedule<F, T>(&self, coroutine: Coroutine<F>) -> T
-//     where
-//         F: FnOnce() -> T,
-//     {
-//         // 你的代码
-//     }
-// }
-
+use std::future::Future;
+use std::pin::{pin, Pin};
+use std::sync::mpsc::{self, Receiver};
 use std::sync::{Arc, Mutex};
+use std::task::{Context, Poll, Wake};
+use std::thread::{self, Thread};
 
-struct Coroutine {
-    task: Mutex<Box<dyn FnMut() + Send + 'static>>,
-    finished: Mutex<bool>,
+struct Coroutine<T> {
+    state: Arc<Mutex<CoroutineState<T>>>,
 }
 
-impl Coroutine {
-    fn new(task: impl FnMut() + Send + 'static) -> Arc<Self> {
-        Arc::new(Coroutine {
-            task: Mutex::new(Box::new(task)),
-            finished: Mutex::new(false),
-        })
-    }
+enum CoroutineState<T> {
+    NotStarted(Box<dyn FnOnce() -> T + Send + 'static>),
+    Running,
+    Completed(T),
+}
 
-    fn run(self: Arc<Self>) {
-        let mut task = self.task.lock().unwrap();
-        let mut finished = self.finished.lock().unwrap();
-        if !*finished {
-            task();
-            *finished = true;
+impl<T> Coroutine<T> {
+    fn new<F>(f: F) -> Self
+    where
+        F: 'static + FnOnce() -> T + Send,
+    {
+        Coroutine {
+            state: Arc::new(Mutex::new(CoroutineState::NotStarted(Box::new(f)))),
         }
     }
+}
 
-    fn is_finished(&self) -> bool {
-        *self.finished.lock().unwrap()
+impl<T> Future for Coroutine<T>
+where
+    T: Send + 'static,
+{
+    type Output = T;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let mut state = self.state.lock().unwrap();
+
+        match &mut *state {
+            CoroutineState::NotStarted(f) => {
+                let f = std::mem::replace(f, Box::new(|| panic!("Coroutine already started")));
+                *state = CoroutineState::Running;
+
+                // Create a waker to wake up the task
+                let waker = cx.waker().clone();
+                let state = Arc::clone(&self.state);
+
+                std::thread::spawn(move || {
+                    let result = f();
+                    let mut state = state.lock().unwrap();
+                    *state = CoroutineState::Completed(result);
+                    waker.wake();
+                });
+
+                Poll::Pending
+            }
+            CoroutineState::Running => Poll::Pending,
+            CoroutineState::Completed(_) => {
+                if let CoroutineState::Completed(result) =
+                    std::mem::replace(&mut *state, CoroutineState::Running)
+                {
+                    Poll::Ready(result)
+                } else {
+                    panic!("CoroutineState changed unexpectedly")
+                }
+            }
+        }
     }
 }
-use std::sync::mpsc::{channel, Receiver, Sender};
-use std::thread::{self, sleep, JoinHandle};
-use std::time::Duration;
 
-struct Scheduler {
-    tx: Sender<Arc<Coroutine>>,
-    handle: JoinHandle<()>,
+struct ThreadWaker(Thread);
+impl Wake for ThreadWaker {
+    // 实现 wake 方法，当唤醒时调用线程的 unpark 方法
+    fn wake(self: Arc<Self>) {
+        self.0.unpark();
+    }
 }
 
-impl Scheduler {
-    fn new() -> Self {
-        let (tx, rx) = channel::<Arc<Coroutine>>();
-        let handle = thread::spawn(move || {
-            while let Ok(coroutine) = rx.recv() {
-                coroutine.run();
+struct CoroutineScheduler {}
+
+impl CoroutineScheduler {
+    fn schedule<F>(coroutine: F) -> Receiver<F::Output>
+    where
+        F: Future + Send + 'static,
+        F::Output: Send,
+    {
+        let (tx, rx) = mpsc::channel();
+        thread::spawn(move || {
+            let mut coroutine = pin!(coroutine);
+            let t = thread::current();
+            let waker = Arc::new(ThreadWaker(t)).into();
+            let mut cx = Context::from_waker(&waker);
+            loop {
+                match coroutine.as_mut().poll(&mut cx) {
+                    Poll::Ready(res) => {
+                        let _ = tx.send(res);
+                        break;
+                    }
+                    Poll::Pending => thread::park(),
+                }
             }
-            println!("scheduler loop quit")
         });
-        Self { tx, handle }
-    }
-
-    fn spawn(&self, task: impl FnMut() + Send + 'static) {
-        let coroutine = Coroutine::new(task);
-        self.tx.send(coroutine);
-    }
-
-    fn join(self, timeout: Option<Duration>) {
-        match timeout {
-            Some(timeout) => {
-                sleep(timeout);
-                drop(self.tx);
-            }
-            None => {
-                self.handle.join();
-            }
-        }
+        rx
     }
 }
 
 fn main() {
-    let scheduler = Scheduler::new();
-
-    scheduler.spawn(|| {
-        for i in 1..=5 {
-            println!("Coroutine 1: {}", i);
-        }
+    let c = Coroutine::new(|| 100);
+    let r1 = CoroutineScheduler::schedule(c);
+    let h1 = thread::spawn(move || {
+        let v = r1.recv().unwrap();
+        println!("Finish 1 {}, value is {v}", chrono::Utc::now());
     });
-
-    scheduler.spawn(|| {
-        for i in 1..=5 {
-            println!("Coroutine 2: {}", i);
-        }
-    });
-
-    scheduler.join(Some(Duration::from_secs(5)))
+    h1.join().unwrap();
 }
