da3cc2486d163bf3101ea8be0000bd9f0d2c4f75
kindywu <kindywu@outlook.com>
2024-07-09 18:51:26 +0800

    commit

--- algorithm/examples/str_word_reverse.rs
+++ algorithm/examples/str_word_reverse.rs
@@ -1,4 +1,46 @@
+#![allow(unused)]
+
 fn main() {
+    // let mut str = String::from("你好");
+    let mut str = String::from("你好 中国");
+
+    let mut cs: Vec<_> = str.chars().collect();
+    let mut start = 0;
+    let mut stop = 0;
+
+    println!("{str}");
+
+    let len = cs.len();
+
+    println!("start: {start}; stop: {stop}; len= {len}");
+
+    for i in 0..len {
+        let c = cs[i];
+        if c == ' ' || i == (len - 1) {
+            stop = if c == ' ' { i - 1 } else { i };
+
+            let size = stop - start;
+            let mid = size / 2;
+            let remain = size % 2;
+
+            println!("start: {start}; stop: {stop}; size: {size}; mid: {mid}; remain: {remain}");
+
+            cs[start..stop + 1].reverse();
+
+            // for j in 0..mid + remain {
+            //     let tmp = arr[start + j];
+            //     arr[start + j] = arr[stop - j];
+            //     arr[stop - j] = tmp;
+            // }
+            start = i + 1;
+        }
+    }
+
+    let str: String = cs.iter().cloned().collect();
+    println!("{:?}", str);
+}
+
+fn method_1() {
     // let mut arr = b"hello".to_vec();
     // let mut arr: Vec<u8> = b"toto".to_vec();
     // let mut arr = b"hello world toto".to_vec();
